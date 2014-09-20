92f0d2ffb492a4c6e07933b61f73f86762ff4434
Miguel Coutada <michaelcoutada@gmail.com>
2014-09-20 03:35:25 +0100

    clean up

--- src/main/java/pt/gov/dgarq/roda/common/convert/db/Main.java
+++ src/main/java/pt/gov/dgarq/roda/common/convert/db/Main.java
@@ -16,7 +16,6 @@ import pt.gov.dgarq.roda.common.convert.db.modules.DatabaseImportModule;
 import pt.gov.dgarq.roda.common.convert.db.modules.db2.in.DB2JDBCImportModule;
 import pt.gov.dgarq.roda.common.convert.db.modules.db2.out.DB2JDBCExportModule;
 import pt.gov.dgarq.roda.common.convert.db.modules.msAccess.in.MsAccessImportModule;
-import pt.gov.dgarq.roda.common.convert.db.modules.msAccess.in.MsAccessUCanAccessImportModule;
 import pt.gov.dgarq.roda.common.convert.db.modules.mySql.in.MySQLJDBCImportModule;
 import pt.gov.dgarq.roda.common.convert.db.modules.mySql.out.MySQLJDBCExportModule;
 import pt.gov.dgarq.roda.common.convert.db.modules.mySql.out.PhpMyAdminExportModule;
@@ -237,15 +236,6 @@ public class Main {
 			} else {
 				logger.error("Wrong argument number for "
 						+ "MSAccess import module: " + importModuleArgs.size());
-			}
-		} else if (importModuleArgs.get(0).equals("MSAccessExp")) {
-			if (importModuleArgs.size() == 2) {
-				importModule = new MsAccessUCanAccessImportModule(new File(
-						importModuleArgs.get(1)));
-			} else {
-				logger.error("Wrong argument number for "
-						+ "MSAccessExp import module: " 
-						+ importModuleArgs.size());
 			}
 //		} else if (importModuleArgs.get(0).equals("ODBC")) {
 //			if (importModuleArgs.size() == 2) {
