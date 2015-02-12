174a2f23e8d6f4aac25ac522a58833c70b2b1af1
Luis Faria <lfaria@keep.pt>
2015-02-12 17:56:59 +0000

    adding test for mysql (not finished)

---
+++ testing/mysql/mysql_test.sql
@@ -0,0 +1,107 @@
+CREATE TABLE datatypes (
+  -- Numeric types
+  col1 TINYINT,
+  col2 SMALLINT,
+  col3 MEDIUMINT,
+  col4 INT,
+  col5 BIGINT,
+  col6 DECIMAL,
+  col7 NUMERIC,
+  col8 FLOAT(20,10),
+  col9 DOUBLE(50,10),
+  col10 BIT(1),
+  col11 BIT(64),
+
+  -- Date and time types
+  col12 DATE,
+  col13 DATETIME,
+  col14 TIMESTAMP,
+  col15 YEAR(4),
+
+  -- String types
+  col16 CHAR(255),
+  col17 VARCHAR(1024),
+  col18 BINARY(255),
+  col19 VARBINARY(1024),
+  col20 TINYBLOB,
+  col21 BLOB,
+  col22 MEDIUMBLOB,
+  col23 LONGBLOB,
+  col24 TINYTEXT,
+  col25 TEXT,
+  col26 MEDIUMTEXT,
+  col27 LONGTEXT,
+  col28 ENUM('small','medium','large'),
+  col29 SET('one','two','three')
+
+  -- XXX: Spatial data types not added
+);
+
+-- ROW with all columns filled
+
+
+-- Numeric types
+-- col1 TINYINT,
+INSERT INTO datatypes(col1) VALUES(1);
+
+-- col2 SMALLINT,
+UPDATE datatypes SET col2 = 123 WHERE col1 = 1;
+
+-- col3 MEDIUMINT,
+UPDATE datatypes SET col3 = 123 WHERE col1 = 1;
+
+-- col4 INT,
+UPDATE datatypes SET col4 = 123 WHERE col1 = 1;
+
+-- col5 BIGINT,
+UPDATE datatypes SET col5 = 123 WHERE col1 = 1;
+
+-- col6 DECIMAL,
+UPDATE datatypes SET col6 = 123 WHERE col1 = 1;
+
+-- col7 NUMERIC,
+UPDATE datatypes SET col7 = 123 WHERE col1 = 1;
+
+-- col8 FLOAT(23,10),
+UPDATE datatypes SET col8 = 12345678901234.56789 WHERE col1 = 1;
+
+-- col9 DOUBLE(53,10),
+UPDATE datatypes SET col9 = 1234567890123456789012345678901234567890123.456789 WHERE col1 = 1;
+
+-- col10 BIT(1),
+UPDATE datatypes SET col10 = b'1' WHERE col1 = 1;
+
+-- col11 BIT(64),
+UPDATE datatypes SET col11 = b'10101010101010101010101010101010101010101010101010101010101' WHERE col1 = 1;
+
+-- Date and time types
+-- col12 DATE,
+
+-- col13 DATETIME,
+-- col14 TIMESTAMP,
+-- col15 YEAR(4),
+
+-- String types
+-- col16 CHAR(255),
+-- col17 VARCHAR(1024),
+-- col18 BINARY(255),
+-- col19 VARBINARY(1024),
+-- col20 TINYBLOB,
+-- col21 BLOB,
+-- col22 MEDIUMBLOB,
+-- col23 LONGBLOB,
+-- col24 TINYTEXT,
+-- col25 TEXT,
+-- col26 MEDIUMTEXT,
+-- col27 LONGTEXT,
+-- col28 ENUM('small','medium','large'),
+-- col29 SET('one','two','three')
+
+-- TODO test max and min numbers
+
+-- ROW with NULLs
+INSERT INTO datatypes(col1) VALUES(2);
+
+
+ALTER TABLE datatypes
+    ADD CONSTRAINT Primary_key PRIMARY KEY (col1);
--- testing/postgresql/postgresql_test.sql
+++ testing/postgresql/postgresql_test.sql
@@ -1,36 +1,3 @@
---
--- PostgreSQL database dump
---
-
-SET statement_timeout = 0;
-SET client_encoding = 'SQL_ASCII';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET search_path = public, pg_catalog;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- Name: datatypes; Type: TABLE; Schema: public; Owner: -; Tablespace:
---
 
 CREATE TABLE datatypes (
     col1 "char" NOT NULL,
@@ -160,15 +127,6 @@ UPDATE datatypes SET col53 = '2015-01-01 23:59:59.999' WHERE col1 = 'a';
 INSERT INTO datatypes(col1) VALUES('b');
 
 
---COPY datatypes (col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15, col16, col17, col18, col19, col20, col21, col22, col23, col24, col25, col26, col27, col29, col30, col31, col32, col33, col34, col35, col36, col37, col38, col39, col40, col41, col42, col43, col44, col45, col46, col47, col48, col49, col50, col51, col52, col53, col54, col55, col56, col57, col58, col59, col60, col61, col62, col63, col64, col65, col66, col67, col68, col69, col70, col71, col72, col73, col74, col75, col76, col77, col78, col79, col80, col81, col82, col83, col84, col85, col86, col87, col88, col89, col90, col91, col92, col93, col94, col95, col96, col97, col98, col99, col100, col101, col102, col103, col104, col105, col106, col107, col108, col109, col110, col111, col112, col113, col114, col115, col116, col117) FROM stdin;
---a	{a,b,c}	1901-12-14 23:59:59-00:36:45	{"1901-12-14 23:59:59-00:36:45"}	\N	\N	922337203685477580	\N	9223372036854775807	1	1	{abc}	{1,0}	t	{t,f}	\N	\N	a	abc	{abc}	{a}	\N	\N	\N	\N	<(1,1),1>	{"<(1,1),1>"}	2015-01-01	{5874897-01-01,0205-12-31}	0.123456789012340001	{0.123456789012340001,0.123456789012340001}	\N	\N	127.0.0.1	{127.0.0.1}	\N	\N	2147483647	{-2147483648,2147483647}	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	1	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
---b	\N	\N	\N	\N	\N	\N	\N	2	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	2	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
---\.
-
-
 ALTER TABLE ONLY datatypes
     ADD CONSTRAINT "Primary key" PRIMARY KEY (col1);
 
---
--- PostgreSQL database dump complete
---
