490c6552c5850c11583494f845609c87a90b2a6c
Andreas Kring <kring@openforce.dk>
2015-08-16 21:38:14 +0200

    ContentExportStrategy: ongoing work on code to generate tableN.xsd

--- src/main/java/dk/magenta/siarddk/SIARDDKsql99ToXsdType.java
+++ src/main/java/dk/magenta/siarddk/SIARDDKsql99ToXsdType.java
@@ -4,8 +4,16 @@ import com.databasepreservation.modules.siard.common.sql99toXSDType;
 
 public class SIARDDKsql99ToXsdType extends sql99toXSDType {
 
-	public static String convert(String sql99Type) {
-		
-		return null;
+  public static String convert(String sql99Type) {
+
+    if (sql99Type.equals("DOUBLE PRECISION")) {
+      return "xs:decimal";
+    } else if (sql99Type.equals("FLOAT")) {
+      return "xs:decimal";
+    } else if (sql99Type.equals("REAL")) {
+      return "xs:decimal";
+    }
+    
+    return null;
 	}
 }
