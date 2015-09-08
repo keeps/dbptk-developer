4391c75d87a810d1cc454a5dcbb1b955c04a0be1
Andreas Kring <andreas@magenta.dk>
2015-09-08 16:29:04 +0200

    ManualMetadata: FileIndexFileStrategy, ongoing work.

--- src/main/java/dk/magenta/siarddk/FileIndexFileStrategy.java
+++ src/main/java/dk/magenta/siarddk/FileIndexFileStrategy.java
@@ -1,7 +1,7 @@
 /**
  * The methods should be called in this order from the SIARDDKMetadataExportStrategy
  * 1) getWriter
- * 2) addFile
+ * 2) addFile (should not be called until writer obtained from the above is closed)
  * 3) generateXML
  */
 package dk.magenta.siarddk;
@@ -10,6 +10,10 @@ import java.io.OutputStream;
 import java.security.DigestOutputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.DatatypeConverter;
 
 import com.databasepreservation.model.exception.ModuleException;
 import com.databasepreservation.model.structure.DatabaseStructure;
@@ -19,9 +23,12 @@ import com.databasepreservation.modules.siard.out.write.WriteStrategy;
 public class FileIndexFileStrategy implements IndexFileStrategy {
 
 	private WriteStrategy writeStrategy;
+	private MessageDigest messageDigest;
+	private Map<String, String> md5sums;
 	
 	public FileIndexFileStrategy(WriteStrategy writeStrategy) {
-		this.writeStrategy = writeStrategy;		
+		this.writeStrategy = writeStrategy;
+		md5sums = new HashMap<String, String>(); 
 	}
 	
 	@Override
@@ -34,28 +41,30 @@ public class FileIndexFileStrategy implements IndexFileStrategy {
 	public OutputStream getWriter(SIARDArchiveContainer outputContainer, String path) throws ModuleException {
 		
 		OutputStream writerFromWriteStrategy = writeStrategy.createOutputStream(outputContainer, path);
-		DigestOutputStream writer = null;
 		try {
-			MessageDigest digest = MessageDigest.getInstance("MD5");
-			writer = new DigestOutputStream(writerFromWriteStrategy, digest);
+			messageDigest = MessageDigest.getInstance("MD5");
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
-		return writer;
+
+		return new DigestOutputStream(writerFromWriteStrategy, messageDigest);
 	}
 	
 	/**
 	 * Adds file to archive.
-	 * @param outputContainer
-	 * @param path
+	 * @param path The path in the outputContainer
 	 * @return md5sum of file
+	 * Pre-condition: writer to calculate md5sum from should be finished and closed.
 	 */
-	public String addFile(SIARDArchiveContainer outputContainer, String path) {
-		// TO-DO:
-		// Make container that the file should be added to
+	public String addFile(String path) {
 		// Calculate md5sum
+		byte[] digest = messageDigest.digest();
+		String md5sum = DatatypeConverter.printHexBinary(digest).toLowerCase();
+
+		// Add file to map
+		md5sums.put(path, md5sum);
 		
-		return null;
+		return md5sum;
 	}
 
 }
