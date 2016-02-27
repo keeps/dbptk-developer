ea3592862e73c37a72084f7e72cfbc2f2c24ae95
Thomas Kristensen <tk@openmodus.dk>
2016-02-27 20:00:30 +0100

    siard-dk: improved a debug message.

--- dbptk-core/src/main/java/com/databasepreservation/modules/siard/in/read/FolderReadStrategyMD5Sum.java
+++ dbptk-core/src/main/java/com/databasepreservation/modules/siard/in/read/FolderReadStrategyMD5Sum.java
@@ -20,11 +20,13 @@ public class FolderReadStrategyMD5Sum extends FolderReadStrategy {
 
   protected Map<DigestInputStream, byte[]> expectedMD5SumForStream;
   protected Map<DigestInputStream, String> pathAssociatedWithStream;
+  protected Map<DigestInputStream, SIARDArchiveContainer> archiveAssociatedWithStream;
 
   public FolderReadStrategyMD5Sum(SIARDArchiveContainer mainContainer) {
     super(mainContainer);
     expectedMD5SumForStream = new HashMap<DigestInputStream, byte[]>();
     pathAssociatedWithStream = new HashMap<DigestInputStream, String>();
+    archiveAssociatedWithStream = new HashMap<DigestInputStream, SIARDArchiveContainer>();
   }
 
   public DigestInputStream createInputStream(SIARDArchiveContainer container, String path, byte[] expectedMD5Sum)
@@ -34,6 +36,7 @@ public class FolderReadStrategyMD5Sum extends FolderReadStrategy {
         MessageDigest.getInstance("MD5"));
       expectedMD5SumForStream.put(digestInputStream, expectedMD5Sum);
       pathAssociatedWithStream.put(digestInputStream, path);
+      archiveAssociatedWithStream.put(digestInputStream, container);
       return digestInputStream;
     } catch (NoSuchAlgorithmException e) {
       throw new ModuleException(e);
@@ -46,11 +49,12 @@ public class FolderReadStrategyMD5Sum extends FolderReadStrategy {
         byte[] expectedMD5Sum = expectedMD5SumForStream.remove(inputStream);
         byte[] computedMD5Sum = inputStream.getMessageDigest().digest();
         String path = pathAssociatedWithStream.remove(inputStream);
+        SIARDArchiveContainer container = archiveAssociatedWithStream.remove(inputStream);
         if (!MessageDigest.isEqual(computedMD5Sum, expectedMD5Sum)) {
           String expectedMD5SumStr = Hex.encodeHexString(expectedMD5Sum);
           String computedMD5SumStr = Hex.encodeHexString(computedMD5Sum);
           throw new ModuleException("Error verifying MD5sum for file [" + path + "]. Computed: " + computedMD5SumStr
-            + " Expected:" + expectedMD5SumStr + " ");
+            + " Expected:" + expectedMD5SumStr + " - during processing of archive [" + container.getPath() + "]");
         }
       } else {
         throw new IllegalArgumentException(
