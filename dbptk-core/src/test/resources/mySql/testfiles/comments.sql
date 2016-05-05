90deada8b687743039162641b0584881e390e7e8
Bruno Ferreira <chalkos@chalkos.net>
2016-05-05 14:59:52 +0100

    bumps postgresql driver version

--- dbptk-core/src/test/resources/mySql/testfiles/comments.sql
+++ dbptk-core/src/test/resources/mySql/testfiles/comments.sql
@@ -4,5 +4,5 @@ CREATE TABLE test_comments (ID INT COMMENT 'this is an integer') COMMENT 'this i
 # SELECT TABLE_SCHEMA, TABLE_NAME, TABLE_COMMENT FROM information_schema.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA = '' AND TABLE_NAME = 'test_comments';
 # SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, COLUMN_COMMENT FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '' AND TABLE_NAME = '' AND COLUMN_NAME = '';
 
-# dummy (and simple to convert) value so dbptk includes the table in the conversion
+# a simple value so dbptk includes the table in the conversion
 INSERT INTO test_comments VALUE (1);
