448e25e74eb5d20f1ce7b6ee4c1aba54ac5d42e6
Bruno Ferreira <chalkos@chalkos.net>
2016-08-18 18:34:53 +0100

    attempt to fix test errors

--- dbptk-model/src/main/java/com/databasepreservation/common/PathInputStreamProvider.java
+++ dbptk-model/src/main/java/com/databasepreservation/common/PathInputStreamProvider.java
@@ -88,9 +88,9 @@ public class PathInputStreamProvider implements InputStreamProvider {
    *           if some IO problem happens. The stream is still closed.
    */
   public PathInputStreamProvider(Path fileLocation) throws ModuleException {
-    if (Files.isReadable(fileLocation)) {
-      throw new ModuleException("Path " + fileLocation.toAbsolutePath().toString() + " is not readable.");
-    }
+//    if (Files.isReadable(fileLocation)) {
+//      throw new ModuleException("Path " + fileLocation.toAbsolutePath().toString() + " is not readable.");
+//    }
     this.path = fileLocation;
     removeTemporaryFileHook = null;
   }
