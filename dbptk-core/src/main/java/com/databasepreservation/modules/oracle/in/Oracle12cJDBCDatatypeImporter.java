94e5c4170c5c654f32bdc0580c8e548f6e5689f6
Bruno Ferreira <chalkos@chalkos.net>
2016-09-07 16:11:29 +0100

    fix #232 (2nd attempt)

--- dbptk-core/src/main/java/com/databasepreservation/modules/oracle/in/Oracle12cJDBCDatatypeImporter.java
+++ dbptk-core/src/main/java/com/databasepreservation/modules/oracle/in/Oracle12cJDBCDatatypeImporter.java
@@ -60,6 +60,10 @@ public class Oracle12cJDBCDatatypeImporter extends JDBCDatatypeImporter {
       type = new SimpleTypeString(2000, true, "CHARSET");
       type.setSql99TypeName("CHARACTER VARYING", 2000);
       type.setSql2008TypeName("CHARACTER VARYING", 2000);
+    } else if ("VARCHAR2".equalsIgnoreCase(typeName)) {
+      type = new SimpleTypeString(2000, true, "CHARSET");
+      type.setSql99TypeName("CHARACTER VARYING", 2000);
+      type.setSql2008TypeName("CHARACTER VARYING", 2000);
     } else if ("NCLOB".equalsIgnoreCase(typeName)) {
       type = new SimpleTypeString(Integer.valueOf(columnSize), true, "CHARSET");
       type.setSql99TypeName("CHARACTER LARGE OBJECT");
