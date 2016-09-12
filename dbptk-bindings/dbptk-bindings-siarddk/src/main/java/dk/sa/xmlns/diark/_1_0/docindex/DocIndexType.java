246f465df349e6ea14c1a4ff5fce73ec9a218dc3
Andreas Kring <andreas@magenta.dk>
2016-09-12 15:35:02 +0200

    Fixed tests and other issues

--- dbptk-bindings/dbptk-bindings-siarddk/src/main/java/dk/sa/xmlns/diark/_1_0/docindex/DocIndexType.java
+++ dbptk-bindings/dbptk-bindings-siarddk/src/main/java/dk/sa/xmlns/diark/_1_0/docindex/DocIndexType.java
@@ -5,23 +5,26 @@
 // Generated on: 2016.09.12 at 02:04:45 PM CEST 
 //
 
-
 package dk.sa.xmlns.diark._1_0.docindex;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
-
 
 /**
  * Index for all documents in the information package
  * 
- * <p>Java class for docIndexType complex type.
+ * <p>
+ * Java class for docIndexType complex type.
  * 
- * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * The following schema fragment specifies the expected content contained within
+ * this class.
  * 
  * <pre>
  * &lt;complexType name="docIndexType"&gt;
@@ -38,41 +41,41 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "docIndexType", propOrder = {
-    "doc"
-})
+@XmlType(name = "docIndexType", propOrder = {"doc"})
+@XmlRootElement(name = "docIndex")
 public class DocIndexType {
 
-    @XmlElement(required = true)
-    protected List<DocumentType> doc;
+  @XmlElement(required = true)
+  protected List<DocumentType> doc;
 
-    /**
-     * Gets the value of the doc property.
-     * 
-     * <p>
-     * This accessor method returns a reference to the live list,
-     * not a snapshot. Therefore any modification you make to the
-     * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the doc property.
-     * 
-     * <p>
-     * For example, to add a new item, do as follows:
-     * <pre>
-     *    getDoc().add(newItem);
-     * </pre>
-     * 
-     * 
-     * <p>
-     * Objects of the following type(s) are allowed in the list
-     * {@link DocumentType }
-     * 
-     * 
-     */
-    public List<DocumentType> getDoc() {
-        if (doc == null) {
-            doc = new ArrayList<DocumentType>();
-        }
-        return this.doc;
+  /**
+   * Gets the value of the doc property.
+   * 
+   * <p>
+   * This accessor method returns a reference to the live list, not a snapshot.
+   * Therefore any modification you make to the returned list will be present
+   * inside the JAXB object. This is why there is not a <CODE>set</CODE> method
+   * for the doc property.
+   * 
+   * <p>
+   * For example, to add a new item, do as follows:
+   * 
+   * <pre>
+   * getDoc().add(newItem);
+   * </pre>
+   * 
+   * 
+   * <p>
+   * Objects of the following type(s) are allowed in the list
+   * {@link DocumentType }
+   * 
+   * 
+   */
+  public List<DocumentType> getDoc() {
+    if (doc == null) {
+      doc = new ArrayList<DocumentType>();
     }
+    return this.doc;
+  }
 
 }
