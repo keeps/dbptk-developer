596ee626844e2312008c0033d9c63a0bf05f3c90
Bruno Ferreira <chalkos@chalkos.net>
2019-02-07 17:22:55 +0000

    Setting version 2.1.0-BETA

--- dbptk-model/src/main/java/com/databasepreservation/model/exception/LicenseNotAcceptedException.java
+++ dbptk-model/src/main/java/com/databasepreservation/model/exception/LicenseNotAcceptedException.java
@@ -1,5 +1,5 @@
 /**
- * The contents of this file are subject to the licenseInfo and copyright
+ * The contents of this file are subject to the license and copyright
  * detailed in the LICENSE file at the root of the source
  * tree and available online at
  *
