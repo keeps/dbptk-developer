72975056797544a5b6de65550f66681fd0dc6db5
mguimaraes <mguimaraes@keep.pt>
2019-04-24 14:12:59 +0100

    Add enhanced support for Sybase ASA DBMS (#349):
    - Add trigger import support for Sybase
    - Add support for view in Sybase
    - Add support for stored procedures for Sybase
    - Add suport for rules in Sybase

--- dbptk-modules/dbptk-module-sybase/src/main/java/com/databasepreservation/modules/sybase/SybaseHelper.java
+++ dbptk-modules/dbptk-module-sybase/src/main/java/com/databasepreservation/modules/sybase/SybaseHelper.java
@@ -11,13 +11,50 @@ public class SybaseHelper extends SQLHelper {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SybaseHelper.class);
 
-  /*@Override
+  @Override
   public String getTriggersSQL(String schemaName, String tableName) {
-    return "select so2.name as TRIGGER_NAME from sysobjects so1, sysobjects so2 where (so2.id = so1.deltrig or so2.id = so1.instrig or so2.id=so1.updtrig or so2.id=so1.seltrig) and so1.name='" + tableName +"'";
-  }*/
+    return "SELECT so2.name AS TRIGGER_NAME FROM sysobjects so1, sysobjects so2 WHERE (so2.id = so1.deltrig OR so2.id = so1.instrig OR so2.id=so1.updtrig OR so2.id=so1.seltrig) AND so1.name='" + tableName + "'";
+  }
+
+  public String getCheckConstraintsSQL(String schemaName, String tableName) {
+    return "SELECT object_name(sys_const.tableid) AS TABLE_NAME, object_name(sys_const.constrid) as CHECK_NAME, sys_comm.text as CHECK_CONDITION" +
+    " FROM sysconstraints sys_const, syscomments sys_comm" +
+    " WHERE sys_const.status=128 AND sys_const.constrid=sys_comm.id AND object_name(sys_const.tableid)='" + tableName + "'";
+  }
 
   public String getTriggerEventSQL(String schemaName, String tableName) {
-    return "select object_name(so2.deltrig) as TRIGGER_DEL, object_name(so2.instrig) as TRIGGER_INS, object_name(so2.updtrig) as TRIGGER_UPD " +
-        "from sysobjects so1, sysobjects so2 where (so2.id = so1.id) and so1.name='" + tableName + "' and (object_name(so2.instrig) is not null or object_name(so2.deltrig) is not null or object_name(so2.updtrig) is not null)";
+    return "SELECT object_name(so2.deltrig) AS TRIGGER_DEL, object_name(so2.instrig) AS TRIGGER_INS, object_name(so2.updtrig) AS TRIGGER_UPD " +
+        "FROM sysobjects so1, sysobjects so2 WHERE (so2.id = so1.id) AND so1.name='" + tableName + "' AND (object_name(so2.instrig) IS NOT NULL OR object_name(so2.deltrig) IS NOT NULL OR object_name(so2.updtrig) IS NOT NULL)";
+  }
+
+  public String getTriggeredActionSQL(String triggerName) {
+    return "SELECT u.name as SCHEMA_NAME, o.name AS TRIGGER_NAME, c.text as TRIGGERED_ACTION " +
+        "FROM sysusers u, syscomments c, sysobjects o " +
+        "WHERE o.type = 'TR' AND o.id = c.id AND o.uid = u.uid AND o.name = '" + triggerName + "'";
+  }
+
+  public String getViewSQL(String viewName) {
+    return "SELECT u.name as SCHEMA_NAME, o.name AS VIEW_NAME, c.text as TEXT " +
+        "FROM sysusers u, syscomments c, sysobjects o " +
+        "WHERE o.type = 'V' AND o.id = c.id AND o.uid = u.uid AND o.name = '" + viewName + "'";
+  }
+
+  public String getProcedureSQL(String procName) {
+    return "SELECT u.name as SCHEMA_NAME, o.name AS PROC_NAME, c.text as TEXT " +
+        "FROM sysusers u, syscomments c, sysobjects o " +
+        "WHERE o.type = 'P' AND o.id = c.id AND o.uid = u.uid AND o.name = '" + procName + "'";
+  }
+
+  public String getRuleNameSQL(String tableName) {
+    return "SELECT DISTINCT(sysobjects.name) AS RULE_NAME" +
+    " FROM sysobjects, syscolumns, syscomments" +
+    " WHERE object_name(syscolumns.domain)=sysobjects.name AND sysobjects.id = syscomments.id" +
+    " AND sysobjects.type='R' AND object_name(syscolumns.id)='" + tableName + "'";
+  }
+
+  public String getRuleSQL(String tableName, String ruleName) {
+    return "SELECT u.name as SCHEMA_NAME, o.name AS RULE_NAME, object_name(syscolumns.id) as TABLE_NAME, c.text as TEXT" +
+        " FROM sysusers u, syscomments c, sysobjects o, syscolumns" +
+        " WHERE o.type = 'R' AND o.id = c.id AND o.uid = u.uid and object_name(syscolumns.domain)=o.name and object_name(syscolumns.id)='" + tableName + "' and o.name='" + ruleName + "'";
   }
 }
--- dbptk-modules/dbptk-module-sybase/src/main/java/com/databasepreservation/modules/sybase/in/SybaseJDBCImportModule.java
+++ dbptk-modules/dbptk-module-sybase/src/main/java/com/databasepreservation/modules/sybase/in/SybaseJDBCImportModule.java
@@ -4,22 +4,23 @@ import com.databasepreservation.model.data.BinaryCell;
 import com.databasepreservation.model.data.Cell;
 import com.databasepreservation.model.data.NullCell;
 import com.databasepreservation.model.exception.ModuleException;
-import com.databasepreservation.model.structure.SchemaStructure;
-import com.databasepreservation.model.structure.Trigger;
+import com.databasepreservation.model.structure.*;
 import com.databasepreservation.model.structure.type.Type;
+import com.databasepreservation.modules.CloseableUtils;
 import com.databasepreservation.modules.jdbc.in.JDBCImportModule;
 import com.databasepreservation.modules.sybase.SybaseHelper;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Miguel Guimarães <mguimaraes@keep.pt>
@@ -108,7 +109,45 @@ public class SybaseJDBCImportModule extends JDBCImportModule {
     return cell;
   }
 
-  /**@Override
+  /**
+   * Gets the check constraints of a given schema table
+   *
+   * @param schemaName
+   * @param tableName
+   * @return
+   */
+  protected List<CheckConstraint> getCheckConstraints(String schemaName, String tableName) throws ModuleException {
+    List<CheckConstraint> checkConstraints = super.getCheckConstraints(schemaName, tableName);
+
+    /* Sybase Rule Binding */
+    String queryForRuleNameSQL = ((SybaseHelper) sqlHelper).getRuleNameSQL(tableName);
+    try (ResultSet res = getStatement().executeQuery(queryForRuleNameSQL)) {
+      while(res.next()) {
+        CheckConstraint c = new CheckConstraint();
+        String ruleName = res.getString("RULE_NAME");
+        String queryForRuleSQL = ((SybaseHelper) sqlHelper).getRuleSQL(tableName, ruleName);
+
+        try (ResultSet rset = getStatement().executeQuery(queryForRuleSQL)) {
+          StringBuilder b = new StringBuilder();
+          while (rset.next()) {
+            b.append(rset.getString("TEXT"));
+          }
+
+          c.setName(ruleName);
+          c.setCondition(b.toString());
+          c.setDescription("Sybase Rule");
+
+          checkConstraints.add(c);
+        }
+      }
+    } catch (SQLException e) {
+      LOGGER.debug("Exception trying to get rule SQL in Sybase", e);
+    }
+
+    return checkConstraints;
+  }
+
+  @Override
   protected List<Trigger> getTriggers(String schemaName, String tableName) throws ModuleException {
     List<Trigger> triggers = new ArrayList<Trigger>();
 
@@ -126,15 +165,6 @@ public class SybaseJDBCImportModule extends JDBCImportModule {
             triggerName = "";
           }
           trigger.setName(triggerName);
-
-          /*String actionTime;
-          try {
-            actionTime = processActionTime(rs.getString("ACTION_TIME"));
-          } catch (SQLException e) {
-            LOGGER.debug("handled SQLException", e);
-            actionTime = "";
-          }
-          trigger.setActionTime(actionTime);
 
           String triggerEvent = "";
 
@@ -168,29 +198,46 @@ public class SybaseJDBCImportModule extends JDBCImportModule {
             LOGGER.error("Error getting trigger event for " + tableName, e);
           }
 
-          System.out.println(triggerEvent);
-
           trigger.setTriggerEvent(triggerEvent);
 
-          String triggeredAction;
-          try {
-            triggeredAction = rs.getString("TRIGGERED_ACTION");
-          } catch (SQLException e) {
-            LOGGER.debug("handled SQLException", e);
+          String triggeredAction = "";
+
+          String queryForTriggeredAction = ((SybaseHelper) sqlHelper).getTriggeredActionSQL(triggerName);
+
+          try (ResultSet res = getStatement().executeQuery(queryForTriggeredAction)) {
+            StringBuilder b = new StringBuilder();
+            while (res.next()) {
+              b.append(res.getString("TRIGGERED_ACTION"));
+            }
+
+            triggeredAction = b.toString();
+          } catch(SQLException | NullPointerException e) {
+            LOGGER.error("Error getting triggered action for " + triggerName, e);
             triggeredAction = "";
           }
+
           trigger.setTriggeredAction(triggeredAction);
 
-          String description;
-          try {
-            description = rs.getString("REMARKS");
-          } catch (SQLException e) {
-            LOGGER.debug("handled SQLException", e);
-            description = null;
+          String actionTime;
+          Pattern pattern;
+          Matcher matcher;
+
+          pattern = Pattern.compile("create trigger (\\S+) on (\\S+) (for|instead of)", Pattern.CASE_INSENSITIVE);
+
+          matcher = pattern.matcher(triggeredAction.replace('\n', ' '));
+
+          if (matcher.find()) {
+            String parsedTriggerName = matcher.group(1);
+            String parsedTableName = matcher.group(2);
+            String parsedActionTime = matcher.group(3);
+
+            actionTime = parsedActionTime.equalsIgnoreCase("for")? "AFTER" : "INSTEAD OF";
+
+          } else {
+            actionTime = "";
           }
-          if (description != null) {
-            trigger.setDescription(description);
-          }
+
+          trigger.setActionTime(actionTime);
 
           triggers.add(trigger);
         }
@@ -202,6 +249,66 @@ public class SybaseJDBCImportModule extends JDBCImportModule {
     }
     return triggers;
   }
-  */
-}
 
+  @Override
+  protected List<ViewStructure> getViews(String schemaName) throws SQLException, ModuleException {
+    List<ViewStructure> views = super.getViews(schemaName);
+    for (ViewStructure v : views) {
+
+      String queryForViewSQL = ((SybaseHelper) sqlHelper).getViewSQL(v.getName());
+      ResultSet rset = null;
+      PreparedStatement statement = null;
+      statement = getConnection().prepareStatement(queryForViewSQL);
+      try {
+        rset = statement.executeQuery();
+        StringBuilder b = new StringBuilder();
+        while (rset.next()) {
+          b.append(rset.getString("TEXT"));
+        }
+
+        v.setQueryOriginal(b.toString());
+      } catch (SQLException e) {
+        LOGGER.debug("Exception trying to get view SQL in Sybase", e);
+      } finally {
+        CloseableUtils.closeQuietly(rset);
+        CloseableUtils.closeQuietly(statement);
+      }
+    }
+    return views;
+  }
+
+  @Override
+  protected List<RoutineStructure> getRoutines(String schemaName) throws SQLException, ModuleException {
+    List<RoutineStructure> routines = new ArrayList<RoutineStructure>();
+
+    ResultSet rset = getMetadata().getProcedures(dbStructure.getName(), schemaName, "%");
+    while (rset.next()) {
+      String routineName = rset.getString(3);
+      RoutineStructure routine = new RoutineStructure();
+      routine.setName(routineName);
+      if (rset.getString(7) != null) {
+        routine.setDescription(rset.getString(7));
+      } else {
+        if (rset.getShort(8) == 1) {
+          routine.setDescription("Routine does not return a result");
+        } else if (rset.getShort(8) == 2) {
+          routine.setDescription("Routine returns a result");
+        }
+      }
+      String queryForProcedureSQL = ((SybaseHelper) sqlHelper).getProcedureSQL(routineName);
+      try (ResultSet res = getStatement().executeQuery(queryForProcedureSQL)) {
+        StringBuilder b = new StringBuilder();
+        while (res.next()) {
+          b.append(res.getString("TEXT"));
+        }
+        routine.setBody(b.toString());
+      } catch (SQLException e) {
+        LOGGER.debug("Could not retrieve routine code (as routine).", e);
+        routine.setBody("");
+      }
+
+      routines.add(routine);
+    }
+    return routines;
+  }
+}
