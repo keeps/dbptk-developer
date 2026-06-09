92eb31fbb26508cfaf68df1b3dff9431bbe33369
Gabriel Barros <g.santos.barros@gmail.com>
2026-06-09 15:30:42 +0100

    Refactor LOB conversion service to support multiple converted files and enhance job status handling

--- dbptk-modules/dbptk-module-siard/src/main/java/com/databasepreservation/modules/siard/services/conversion/ConversionResult.java
+++
@@ -1,9 +0,0 @@
-package com.databasepreservation.modules.siard.services.conversion;
-
-import java.nio.file.Path;
-
-/**
- * @author Gabriel Barros <gbarros@keep.pt>
- */
-public record ConversionResult(Path convertedFile, Path reportFile) {
-}
