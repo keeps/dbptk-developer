e568583f23fedabde43c7b4401dd86af6c76ffe0
Gabriel Barros <g.santos.barros@gmail.com>
2026-06-22 11:04:21 +0100

    Add ComplianceStatus enum and enhance report models with additional properties

---
+++ dbptk-modules/dbptk-module-siard/src/main/java/com/databasepreservation/modules/siard/services/conversion/model/report/ComplianceStatus.java
@@ -0,0 +1,8 @@
+package com.databasepreservation.modules.siard.services.conversion.model.report;
+
+/**
+ * @author Gabriel Barros <gbarros@keep.pt>
+ */
+public enum ComplianceStatus {
+  PASSED, PARTIAL, FAILED
+}
