19c825a32af6a5a8894da282934fe015ed3ace6c
Gabriel Barros <g.santos.barros@gmail.com>
2026-06-22 11:04:21 +0100

    Add ComplianceStatus enum and enhance report models with additional properties

--- dbptk-modules/dbptk-module-siard/src/main/java/com/databasepreservation/modules/siard/services/conversion/model/report/ArtifactReport.java
+++ dbptk-modules/dbptk-module-siard/src/main/java/com/databasepreservation/modules/siard/services/conversion/model/report/ArtifactReport.java
@@ -3,11 +3,15 @@ package com.databasepreservation.modules.siard.services.conversion.model.report;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * @author Gabriel Barros <gbarros@keep.pt>
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record ArtifactReport(String logicalName, String originalMimeType, String finalMimeType, boolean isBypassed,
-  List<String> formatHistory, List<AuditTrailStep> auditTrail, String errorMessage) {
+public record ArtifactReport(@JsonProperty("logicalName") String logicalName,
+  @JsonProperty("originalMimeType") String originalMimeType, @JsonProperty("finalMimeType") String finalMimeType,
+  @JsonProperty("isBypassed") boolean isBypassed, @JsonProperty("complianceStatus") ComplianceStatus complianceStatus,
+  @JsonProperty("formatHistory") List<String> formatHistory,
+  @JsonProperty("auditTrail") List<AuditTrailStep> auditTrail, @JsonProperty("errorMessage") String errorMessage) {
 }
--- dbptk-modules/dbptk-module-siard/src/main/java/com/databasepreservation/modules/siard/services/conversion/model/report/ConversionReport.java
+++ dbptk-modules/dbptk-module-siard/src/main/java/com/databasepreservation/modules/siard/services/conversion/model/report/ConversionReport.java
@@ -2,16 +2,21 @@ package com.databasepreservation.modules.siard.services.conversion.model.report;
 
 import java.util.List;
 
+import com.databasepreservation.modules.siard.services.conversion.model.JobStatus;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * @author Gabriel Barros <gbarros@keep.pt>
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record ConversionReport(String jobId, String status, String originalInputFile, Integer totalArtifactsProduced,
-  List<ArtifactReport> artifacts, String errorMessage, DbptkContext dbptkContext) {
+public record ConversionReport(@JsonProperty("jobId") String jobId, @JsonProperty("status") JobStatus status,
+  @JsonProperty("originalFilename") String originalFilename,
+  @JsonProperty("totalArtifactsProduced") Integer totalArtifactsProduced,
+  @JsonProperty("artifacts") List<ArtifactReport> artifacts, @JsonProperty("errorMessage") String errorMessage,
+  @JsonProperty("dbptkContext") DbptkContext dbptkContext) {
   public ConversionReport withContext(DbptkContext context) {
-    return new ConversionReport(jobId, status, originalInputFile, totalArtifactsProduced, artifacts, errorMessage,
+    return new ConversionReport(jobId, status, originalFilename, totalArtifactsProduced, artifacts, errorMessage,
       context);
   }
 }
